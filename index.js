8a5faeecaccd9d85c9c2de076aadc9a0321f4877
sin wan wildy <sw.wildy@gmail.com>
2021-02-23 18:53:03 -0800

    commit tree d589be8ce6851d5a4d833f9e9791720c157d3a25

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+// document.addEventListener("keydown", function(e)
+// {
+//     if (e.key === "ArrowLeft") 
+//     {
+//         let leftNumbers = dodger.style.left.replace("px", "");
+//         let left = parseInt(leftNumbers, 10);
+//         dodger.style.left = `${left - 1}px`;
+//     }
+// });
+
+
+
+let dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function(e)
+{
+    if (e.key === "ArrowLeft")
+    {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight")
+    {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft()
+{
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0)
+    {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight()
+{
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0)
+    {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
